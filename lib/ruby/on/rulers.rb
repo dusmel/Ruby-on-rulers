53f499d52959e5ba3ad22126884fbfc90301eff2
hadad <hadadus@gmail.com>
2022-04-24 23:01:08 +0200

    initialize the app with bundler

---
+++ lib/ruby/on/rulers.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "rulers/version"
+
+module Ruby
+  module On
+    module Rulers
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
